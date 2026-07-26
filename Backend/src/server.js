982033bb7f0d99f3f324cd62ea98120005b54f79
mirajHossen99 <mirajhossen211013@gmail.com>
2026-07-27 03:42:39 +0600

    import post-route

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 
 
 dotenv.config();
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
 // health check
 app.get("/api/health", (_req, res) => res.json({ status: "ok" }));
